da642d8f6372c5f57c3bbf39bef163ee86bcd9f7
Erik Unger <erik@erikunger.com>
2021-10-10 20:25:46 +0200

    added TypeFormatters.WithOtherFormatter

--- typeformatters.go
+++ typeformatters.go
@@ -20,53 +20,27 @@ func (f *TypeFormatters) FormatCell(ctx context.Context, cell *Cell) (str string
 	if f == nil {
 		return "", false, ErrNotSupported
 	}
-	if tw, ok := f.Types[cell.Value.Type()]; ok {
-		str, raw, err := tw.FormatCell(ctx, cell)
+	if typeFmt, ok := f.Types[cell.Value.Type()]; ok {
+		str, raw, err := typeFmt.FormatCell(ctx, cell)
 		if !errors.Is(err, ErrNotSupported) {
 			return str, raw, err
 		}
 	}
-	for it, iw := range f.InterfaceTypes {
-		if cell.Value.Type().Implements(it) {
-			str, raw, err := iw.FormatCell(ctx, cell)
+	for interfaceType, interfaceFmt := range f.InterfaceTypes {
+		if cell.Value.Type().Implements(interfaceType) {
+			str, raw, err := interfaceFmt.FormatCell(ctx, cell)
 			if !errors.Is(err, ErrNotSupported) {
 				return str, raw, err
 			}
 		}
 	}
-	if kw, ok := f.Kinds[cell.Value.Kind()]; ok {
-		return kw.FormatCell(ctx, cell)
+	if kindFmt, ok := f.Kinds[cell.Value.Kind()]; ok {
+		return kindFmt.FormatCell(ctx, cell)
 	}
 	if f.Other != nil {
 		return f.Other.FormatCell(ctx, cell)
 	}
 	return "", false, ErrNotSupported
-}
-
-func (f *TypeFormatters) cloneOrNew() *TypeFormatters {
-	if f == nil {
-		return new(TypeFormatters)
-	}
-	c := &TypeFormatters{Other: f.Other}
-	if len(f.Types) > 0 {
-		c.Types = make(map[reflect.Type]CellFormatter, len(f.Types))
-		for key, val := range f.Types {
-			c.Types[key] = val
-		}
-	}
-	if len(f.InterfaceTypes) > 0 {
-		c.InterfaceTypes = make(map[reflect.Type]CellFormatter, len(f.InterfaceTypes))
-		for key, val := range f.InterfaceTypes {
-			c.InterfaceTypes[key] = val
-		}
-	}
-	if len(f.Kinds) > 0 {
-		c.Kinds = make(map[reflect.Kind]CellFormatter, len(f.Kinds))
-		for key, val := range f.Kinds {
-			c.Kinds[key] = val
-		}
-	}
-	return c
 }
 
 func (f *TypeFormatters) WithTypeFormatter(typ reflect.Type, fmt CellFormatter) *TypeFormatters {
@@ -110,4 +84,36 @@ func (f *TypeFormatters) WithKindFormatter(kind reflect.Kind, fmt CellFormatter)
 	}
 	mod.Kinds[kind] = fmt
 	return mod
+}
+
+func (f *TypeFormatters) WithOtherFormatter(fmt CellFormatter) *TypeFormatters {
+	mod := f.cloneOrNew()
+	mod.Other = fmt
+	return mod
+}
+
+func (f *TypeFormatters) cloneOrNew() *TypeFormatters {
+	if f == nil {
+		return new(TypeFormatters)
+	}
+	c := &TypeFormatters{Other: f.Other}
+	if len(f.Types) > 0 {
+		c.Types = make(map[reflect.Type]CellFormatter, len(f.Types))
+		for key, val := range f.Types {
+			c.Types[key] = val
+		}
+	}
+	if len(f.InterfaceTypes) > 0 {
+		c.InterfaceTypes = make(map[reflect.Type]CellFormatter, len(f.InterfaceTypes))
+		for key, val := range f.InterfaceTypes {
+			c.InterfaceTypes[key] = val
+		}
+	}
+	if len(f.Kinds) > 0 {
+		c.Kinds = make(map[reflect.Kind]CellFormatter, len(f.Kinds))
+		for key, val := range f.Kinds {
+			c.Kinds[key] = val
+		}
+	}
+	return c
 }
