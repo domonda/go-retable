8fff236973e3c08b05c3c548e72337d89d4185c2
Erik Unger <erik@erikunger.com>
2024-01-22 23:05:19 +0100

    remove local test

--- exceltable/read_test.go
+++ exceltable/read_test.go
@@ -1,13 +1,7 @@
 package exceltable
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
-func TestReadLocalFile(t *testing.T) {
-	gotSheets, err := ReadLocalFile("/Users/erik/Downloads/conthaus/Kontodaten.xlsx", true)
-	require.NoError(t, err)
-	require.Len(t, gotSheets, 1)
-}
+// func TestReadLocalFile(t *testing.T) {
+// 	gotSheets, err := ReadLocalFile("example.xlsx", true)
+// 	require.NoError(t, err)
+// 	require.Len(t, gotSheets, 1)
+// }
